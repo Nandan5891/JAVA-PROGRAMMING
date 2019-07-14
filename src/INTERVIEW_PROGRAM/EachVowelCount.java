6444470e7c4f1edf445d9d36c8c9d29af6214330
nandansoftechindia <self.ram100@gmail.com>
2019-07-14 15:08:11 +0530

    Commint VowelCount

---
+++ src/INTERVIEW_PROGRAM/EachVowelCount.java
@@ -0,0 +1,26 @@
+package INTERVIEW_PROGRAM;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EachVowelCount {
+    public static void main(String[] args) {
+        String str = "javaessentials";
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        char[] chArray = str.toCharArray();
+        for (char c : chArray) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        for(Map.Entry<Character,Integer>entry:map.entrySet())
+        {
+            if(entry.getKey()=='a' || entry.getKey()=='e'|| entry.getKey()=='i'||entry.getKey()=='o'||entry.getKey()=='u');
+            {
+                System.out.println(entry.getKey()+ " -->"+entry.getValue());
+            }
+        }
+    }
+}
